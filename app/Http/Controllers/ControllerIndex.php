508041524260dac2a0b08d984f3f2f48c3562a4e
Aguirrido <45271970+Aguirrido@users.noreply.github.com>
2020-04-23 21:07:17 -0500

    pruebas

--- app/Http/Controllers/ControllerIndex.php
+++ app/Http/Controllers/ControllerIndex.php
@@ -3,11 +3,42 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use SoapClient;
 
 class ControllerIndex extends Controller
 {
       public function index()
     {
         return view('inicio');
+    }
+
+    public function soap()
+    {
+        try {
+            $opts = array(
+                'http' => array(
+                    'user_agent' => 'PHPSoapClient'
+                )
+            );
+            $context = stream_context_create($opts);
+
+            $wsdlUrl = "http://187.191.95.87:8090/WSCPreDocumentacion.asmx";
+            $soapClientOptions = array(
+                'stream_context' => $context,
+            );
+
+            $client = new SoapClient($wsdlUrl, $soapClientOptions);
+
+            $checkVatParameters = array(
+                'K_Guia'=>'1103378060','K_Usuario'=>'730','K_Cliente'=>'14685','Token'=>'-1','strReferencia'=>''
+            );
+
+            dd($result = $client->GetRastreoxGuia($checkVatParameters));
+
+        }
+        catch(\Exception $e) {
+            echo $e->getMessage();
+        }
+
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -13,5 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 Route::get('/', 'ControllerIndex@index');
+Route::get('/soap', 'ControllerIndex@soap');
 
 
