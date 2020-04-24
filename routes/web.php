14c5f1101d857bbea0c9078701e3d0d113caebe4
Aguirrido <45271970+Aguirrido@users.noreply.github.com>
2020-04-23 19:55:49 -0500

    .

--- routes/web.php
+++ routes/web.php
@@ -14,20 +14,4 @@ use Illuminate\Support\Facades\Route;
 */
 Route::get('/', 'ControllerIndex@index');
 
-Route::get('/enduro/clima', function () {
-    $opts = array(
-        'ssl' => array('ciphers'=>'RC4-SHA', 'verify_peer'=>false, 'verify_peer_name'=>false)
-    );
 
-    $params = array ('encoding' => 'UTF-8', 'verifypeer' => false, 'verifyhost' => false, 'soap_version' => , 'trace' => 1, 'exceptions' => 1, "connection_timeout" => 180, 'stream_context' => stream_context_create($opts) );
-    $url = "http://www.webservicex.net/globalweather.asmx?WSDL";
-
-    try{
-        $client = new SoapClient($url,$params);
-        dd($client->GetRastreoxGuia(['CountryName' => 'Peru'])->GetCitiesByCountryResult);
-    }
-    catch(SoapFault $fault) {
-        echo '<br>'.$fault;
-    }
-
-});
